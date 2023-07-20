1be65b2ec758d2ccc4adae9e9e7ae9aaa4a49c47
BogdanZhyck <zhyck.bogdan@gmail.com>
2023-07-20 14:26:39 +0300

    HomeWork2_2

---
+++ Home Work2/HomeWork2_2/HomeWork2_2/OrderService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2_2
+{
+	public class OrderService
+	{
+		private static int orderNumber = 1;
+
+		public void RegisterOrder(ShoppingCart cart)
+		{
+			Console.WriteLine("Зареєстровано замовлення з номером: " + orderNumber);
+			orderNumber++;
+		}
+	}
+}
---
+++ Home Work2/HomeWork2_2/HomeWork2_2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HomeWork2_2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var products = new Product[3];
+			products[0] = new Product { Id = 1, Name = "Товар 1", Price = 10.99m };
+			products[1] = new Product { Id = 2, Name = "Товар 2", Price = 19.99m };
+			products[2] = new Product { Id = 3, Name = "Товар 3", Price = 5.99m };
+
+			var cart = new ShoppingCart();
+
+			for (int i = 0; i < Math.Min(10, products.Length); i++)
+			{
+				cart.AddToCart(products[i]);
+			}
+
+			cart.PlaceOrder();
+
+			var orderService = new OrderService();
+			orderService.RegisterOrder(cart);
+		}
+	}
+}
